3642306057cb68ed9e32e686eebbbc5baff720ae
David Robinson <dr6@sanger.ac.uk>
2017-07-21 16:44:16 +0100

    changed PermissionChecker to use pluck instead of select/distinct/map

--- app/lib/permission_checker.rb
+++ app/lib/permission_checker.rb
@@ -7,8 +7,8 @@ class PermissionChecker
 
     def check(permission_type, names, material_uuids)
       @unpermitted_uuids = []
-      stamp_ids = AkerPermissionGem::Permission.select(:accessible_id).where(permission_type: permission_type, permitted: names).distinct.map(&:accessible_id)
-      permitted_uuids = Set.new(StampMaterial.select(:material_uuid).where(material_uuid: material_uuids, stamp_id: stamp_ids).map(&:material_uuid))
+      stamp_ids = AkerPermissionGem::Permission.where(permission_type: permission_type, permitted: names).pluck('distinct accessible_id')
+      permitted_uuids = Set.new(StampMaterial.where(material_uuid: material_uuids, stamp_id: stamp_ids).pluck('distinct material_uuid'))
 
       @unpermitted_uuids = material_uuids.reject {|mu| permitted_uuids.include?(mu) }
       return @unpermitted_uuids.empty?
