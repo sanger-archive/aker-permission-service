056355d69f5cb6fe97b29c78026d364cfad17d83
David Robinson <dr6@sanger.ac.uk>
2017-07-24 09:06:50 +0100

    refactored PermissionChecker to pull results in one query

--- app/lib/permission_checker.rb
+++ app/lib/permission_checker.rb
@@ -7,14 +7,21 @@ class PermissionChecker
 
     def check(permission_type, names, material_uuids)
       @unpermitted_uuids = []
-      stamp_ids = AkerPermissionGem::Permission.where(permission_type: permission_type, permitted: names).pluck('distinct accessible_id')
-      permitted_uuids = Set.new(StampMaterial.where(material_uuid: material_uuids, stamp_id: stamp_ids).pluck('distinct material_uuid'))
+      permitted_uuids = Set.new(
+        StampMaterial.where(material_uuid: material_uuids, stamp_id: select_permitted_stamp_ids(permission_type, names)).
+                      pluck('distinct material_uuid')
+      )
 
-      @unpermitted_uuids = material_uuids.reject {|mu| permitted_uuids.include?(mu) }
+      @unpermitted_uuids = material_uuids.reject { |mu| permitted_uuids.include?(mu) }
       return @unpermitted_uuids.empty?
     end
 
-  end
+  private
 
+    def select_permitted_stamp_ids(permission_type, names)
+      AkerPermissionGem::Permission.select(:accessible_id).where(permission_type: permission_type, permitted: names).distinct()
+    end
+
+  end
 
 end
