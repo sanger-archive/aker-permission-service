81db2ef673c867f0b8d8fb556f1c65dd958cdbea
Harriet Craven <hc6@sanger.ac.uk>
2017-07-27 09:13:28 +0100

    Removed after_destroy delete_all permission as this is now done in the permissions gem

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -3,10 +3,6 @@ class Stamp < ApplicationRecord
 
   has_many :stamp_materials, dependent: :delete_all
 
-  after_destroy do
-    permissions.destroy_all
-  end
-
   validates :name, presence: true, uniqueness: true
   validates :owner_id, presence: true
 end
