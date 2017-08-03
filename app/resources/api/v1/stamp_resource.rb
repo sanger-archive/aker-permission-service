755ccebd02ee23e51be2039ca60f71f604183538
Harriet Craven <hc6@sanger.ac.uk>
2017-08-03 12:31:41 +0100

    Added filter by user

--- app/resources/api/v1/stamp_resource.rb
+++ app/resources/api/v1/stamp_resource.rb
@@ -6,6 +6,12 @@ module Api
       has_many :permissions, class_name: 'Permission', relation_name: :permissions
       has_many :materials, class_name: 'Material', relation_name: :stamp_materials
 
+      # http://localhost:3000/api/v1/sets?filter[owner_id]=guest
+      filter :owner_id, apply: -> (records, value, _options) {
+        return records.none if value.nil?
+        records.where(owner_id: value)
+      }
+
       before_create do
         @model.owner_id = context[:current_user].email
       end
