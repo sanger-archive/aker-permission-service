fcab9064c63958820e5277ce037fab84c1eec60b
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-11-02 16:10:19 +0000

    Improve deputy factory

--- spec/factories/deputies.rb
+++ spec/factories/deputies.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :deputy do
-    user_email "MyString"
-    deputy "MyString"
+    user_email "boss@sanger.ac.uk"
+    sequence :deputy do |n|
+      "deputy#{n}@sanger.ac.uk"
+    end
   end
 end
