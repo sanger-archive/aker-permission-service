6e12522f907e4728dabc55a7f9c33ddac7998841
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-01-19 12:07:43 +0000

    Implement deputy permission checks

--- app/lib/permission_checker.rb
+++ app/lib/permission_checker.rb
@@ -1,37 +1,63 @@
 class PermissionChecker
-
   class << self
     attr_reader :unpermitted_uuids
 
+    # Returns a boolean to indicate whether 'names' (consisting of a Sanger user
+    # and their LDAP groups) have the 'permission_type' on the materials
+    # represented by the IDs in 'material_uuids'
+    # As long as the user or any of the groups have the 'permission_type' over
+    # all of the materials, this will return true. False otherwise.
     def check(permission_type, names, material_uuids)
       material_uuids = material_uuids.uniq
       @unpermitted_uuids = []
-      permitted_uuids = select_permitted_material_uuids(permission_type, names, material_uuids)
-
+      permitted_uuids = select_permitted_material_uuids(permission_type, names,
+                                                        material_uuids)
       @unpermitted_uuids = material_uuids - permitted_uuids
       @unpermitted_uuids -= owned_material_uuids(@unpermitted_uuids, names)
+      @unpermitted_uuids -= deputised_material_uuids(@unpermitted_uuids, names)
       return @unpermitted_uuids.empty?
     end
 
-  private
+    private
 
     def select_permitted_material_uuids(permission_type, names, material_uuids)
-      AkerPermissionGem::Permission.
-        joins('JOIN stamps ON (accessible_id=stamps.id)').
-        joins('JOIN stamp_materials ON (stamps.id=stamp_materials.stamp_id)').
-        where(stamps: { deactivated_at: nil },
-              permissions: { permitted: names, permission_type: permission_type },
-              stamp_materials: { material_uuid: material_uuids }).
-        pluck('distinct material_uuid')
+      AkerPermissionGem::Permission
+        .joins('JOIN stamps ON (accessible_id=stamps.id)')
+        .joins('JOIN stamp_materials ON (stamps.id=stamp_materials.stamp_id)')
+        .where(stamps: { deactivated_at: nil },
+               permissions: { permitted: names,
+                              permission_type: permission_type },
+               stamp_materials: { material_uuid: material_uuids })
+        .pluck('distinct material_uuid')
     end
 
+    # Returns an array of material IDs that the user is authorised to 'consume'
+    # (order work on) as they are the owner (Sample Guardian) of that material
     def owned_material_uuids(material_uuids, names)
-      MatconClient::Material.where(
-        _id: {"$in" => material_uuids},
-        owner_id: {"$in" => names}
-      ).select(:_id).map { |m| m._id }
+      puts MatconClient::Material.where(
+        _id: { '$in' => material_uuids },
+        owner_id: { '$in' => names }
+      ).select(:_id).map(&:_id)
     end
 
+    # Returns an array of material IDs that the user is authorised to 'consume'
+    # (order work on) by being a deputy of the material Sample Guardian
+    def deputised_material_uuids(material_uuids, names)
+      # Get materials that need checking for user consume permission
+      materials = MatconClient::Material.where(_id: { '$in' => material_uuids })
+                                        .map do |material|
+                                          [material._id, material.owner_id]
+                                        end
+      return [] if materials.empty?
+      # Extract Sample Guardian email addresses
+      sample_guardian_emails = materials.collect { |mat| mat[1] }.uniq
+      # Get the Sample Guardian emails that the user is a deputy of
+      user_deputy_of = Deputy.where(user_email: sample_guardian_emails,
+                                    deputy: names).pluck(:user_email)
+      # Remove materials that the user isn't authorised to order work on
+      user_unauthorised_for = sample_guardian_emails - user_deputy_of
+      materials.delete_if { |mat| user_unauthorised_for.include?(mat[1]) }
+               .collect { |mat| mat[0] }
+    end
   end
-
 end
--- spec/lib/permission_checker_spec.rb
+++ spec/lib/permission_checker_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe PermissionChecker do
   def make_stamp(permitted, permission_type)
     stamp = create(:stamp)
-    stamp.permissions.create(permitted: permitted, permission_type: permission_type)
+    stamp.permissions.create(permitted: permitted,
+                             permission_type: permission_type)
     create(:stamp_material, stamp: stamp)
     stamp
   end
@@ -13,78 +14,93 @@ RSpec.describe PermissionChecker do
     @material_owners[uuid] = owner
   end
 
-  let(:response_headers) { {'Content-Type' => 'application/json'} }
+  let(:response_headers) { { 'Content-Type' => 'application/json' } }
 
   let(:json_schema) do
     {
-      required: ["supplier_name", "scientific_name", "gender", "donor_id", "phenotype"],
-      type:"object",
+      required: %w[supplier_name scientific_name gender donor_id phenotype],
+      type: 'object',
       properties: {
-        available: {default:false, required:false, type:"boolean"},
-        hmdmc_not_required_confirmed_by: {not_blank:true, required:false, type:"string"},
-        scientific_name: {required:true, type:"string", allowed:["Homo sapiens", "Mus musculus"]},
-        gender: {required:true, type:"string", allowed:["male", "female", "unknown"]},
-        date_of_receipt: {type:"string", format:"date"},
-        material_type: {type:"string", allowed:["blood", "dna"]},
-        hmdmc_set_by: {not_blank:true, required:false, type:"string", required_with_hmdmc:true},
-        hmdmc: {hmdmc_format:true, type:"string", required:false},
-        donor_id: {required:true, type:"string"},
-        phenotype: {required:true, type:"string"},
-        supplier_name: {required:true, type:"string"},
+        available: { default: false, required: false, type: 'boolean' },
+        hmdmc_not_required_confirmed_by: { not_blank: true, required: false,
+                                           type: 'string' },
+        scientific_name: { required: true, type: 'string',
+                           allowed: ['Homo sapiens', 'Mus musculus'] },
+        gender: { required: true, type: 'string',
+                  allowed: %w[male female unknown] },
+        date_of_receipt: { type: 'string', format: 'date' },
+        material_type: { type: 'string', allowed: %w[blood dna] },
+        hmdmc_set_by: { not_blank: true, required: false, type: 'string',
+                        required_with_hmdmc: true },
+        hmdmc: { hmdmc_format: true, type: 'string', required: false },
+        donor_id: { required: true, type: 'string' },
+        phenotype: { required: true, type: 'string' },
+        supplier_name: { required: true, type: 'string' },
+        owner_id: { required: true, type: 'string' }
       }
     }.to_json
   end
 
   def stub_materials_service
-    stub_request(:get, Rails.application.config.material_url+'/materials/json_schema').
+    stub_request(:get, Rails.application.config.material_url + '/materials/json_schema').
       to_return(status: 200, body: json_schema, headers: response_headers)
 
-    @material_owners ||= {}
-    stub_request(:post, Rails.application.config.material_url+'/materials/search').
-      to_return do |request|
+    stub_request(:post, Rails.application.config.material_url + '/materials/search')
+      .to_return do |request|
         body = JSON.parse(request.body)
         where = body['where']
         ids = where['_id']['$in']
-        owners = where['owner_id']['$in']
-        ids = ids.select { |id| owners.include?(@material_owners[id]) }
-        result = { _items: ids.map { |id| { _id: id } } }
-
+        if where.include?('owner_id')
+          # Request made by owned_material_uuids
+          owners = where['owner_id']['$in']
+          ids = ids.select { |id| owners.include?(@material_owners[id]) }
+          result = { _items: ids.map { |id| { _id: id } } }
+        else
+          # Request made by deputised_material_uuids
+          result = { _items: ids.map { |id| { _id: id, owner_id: @material_owners[id] } } }
+        end
         { status: 200, body: result.to_json, headers: response_headers }
       end
-   end
+  end
 
   describe '#check' do
     before do
-      @stamp1 = make_stamp('jeff', :edit)
-      @stamp2 = make_stamp('beta', :edit)
-      @stamp3 = make_stamp('jeff', :read)
-      @stamp4 = make_stamp('dirk', :edit)
+      @stamp1 = make_stamp('jeff@sanger.ac.uk', :edit)
+      @stamp2 = make_stamp('beta@sanger.ac.uk', :edit)
+      @stamp3 = make_stamp('jeff@sanger.ac.uk', :read)
+      @stamp4 = make_stamp('dirk@sanger.ac.uk', :edit)
 
-      @permitted_uuids = [@stamp1.stamp_materials.first.material_uuid, @stamp2.stamp_materials.first.material_uuid]
-      @unpermitted_uuids = [@stamp3.stamp_materials.first.material_uuid, @stamp4.stamp_materials.first.material_uuid, SecureRandom.uuid]
+      @permitted_uuids = [@stamp1.stamp_materials.first.material_uuid,
+                          @stamp2.stamp_materials.first.material_uuid]
+      @unpermitted_uuids = [@stamp3.stamp_materials.first.material_uuid,
+                            @stamp4.stamp_materials.first.material_uuid,
+                            SecureRandom.uuid]
       @material_uuids = @permitted_uuids + @unpermitted_uuids
+
+      # All material UUIDs must also have an owner (sample guardian)
+      @material_uuids.each { |uuid| set_material_owner(uuid, 'default@sanger.ac.uk') }
 
       stub_materials_service
     end
 
     context 'when the materials are not all permitted' do
       it 'should return false' do
-        expect(PermissionChecker.check(:edit, ['alpha', 'beta', 'jeff'], @material_uuids)).to eq(false)
+        expect(PermissionChecker.check(:edit, ['alpha@sanger.ac.uk', 'beta@sanger.ac.uk', 'jeff@sanger.ac.uk'], @material_uuids)).to eq(false)
       end
 
       it 'should put the unpermitted materials into the unpermitted_uuids attribute' do
-        PermissionChecker.check(:edit, ['alpha', 'beta', 'jeff'], @material_uuids)
+        PermissionChecker.check(:edit, ['alpha@sanger.ac.uk', 'beta@sanger.ac.uk', 'jeff@sanger.ac.uk'], @material_uuids)
         expect(PermissionChecker.unpermitted_uuids).to eq(@unpermitted_uuids)
       end
     end
 
     context 'when the materials are all permitted' do
-      it 'should return false' do
-        expect(PermissionChecker.check(:edit, ['alpha', 'beta', 'jeff'], @permitted_uuids)).to eq(true)
+      it 'should return true' do
+        expect(PermissionChecker.check(:edit, ['alpha@sanger.ac.uk', 'beta@sanger.ac.uk', 'jeff@sanger.ac.uk'], @permitted_uuids)).to eq(true)
       end
 
       it 'should have an empty unpermitted_uuids attribute' do
-        PermissionChecker.check(:edit, ['alpha', 'beta', 'jeff'], @permitted_uuids)
+        PermissionChecker.check(:edit, ['alpha@sanger.ac.uk', 'beta@sanger.ac.uk', 'jeff@sanger.ac.uk'], @permitted_uuids)
         expect(PermissionChecker.unpermitted_uuids).to be_empty
       end
     end
@@ -97,11 +113,11 @@ RSpec.describe PermissionChecker do
       end
 
       it 'should return false' do
-        expect(PermissionChecker.check(:edit, ['alpha', 'beta', 'jeff'], @material_uuids)).to eq(false)
+        expect(PermissionChecker.check(:edit, ['alpha@sanger.ac.uk', 'beta@sanger.ac.uk', 'jeff@sanger.ac.uk'], @material_uuids)).to eq(false)
       end
 
       it 'should put the unpermitted materials into the unpermitted_uuids attribute' do
-        PermissionChecker.check(:edit, ['alpha', 'beta', 'jeff'], @material_uuids)
+        PermissionChecker.check(:edit, ['alpha@sanger.ac.uk', 'beta@sanger.ac.uk', 'jeff@sanger.ac.uk'], @material_uuids)
         expect(PermissionChecker.unpermitted_uuids).to eq(@unpermitted_uuids)
       end
     end
@@ -112,11 +128,13 @@ RSpec.describe PermissionChecker do
       before do
         uuids.each { |uuid| set_material_owner(uuid, owner) }
       end
+
       it 'should return true' do
-        expect(PermissionChecker.check(:edit, ['xyz', owner, 'zyx'], uuids)).to eq(true)
+        expect(PermissionChecker.check(:edit, ['xyz@sanger.ac.uk', owner, 'zyx@sanger.ac.uk'], uuids)).to eq(true)
       end
+
       it 'should have an empty unpermitted_uuids attribute' do
-        PermissionChecker.check(:edit, ['xyz', owner, 'zyx'], uuids)
+        PermissionChecker.check(:edit, ['xyz@sanger.ac.uk', owner, 'zyx@sanger.ac.uk'], uuids)
         expect(PermissionChecker.unpermitted_uuids).to be_empty
       end
     end
@@ -127,15 +145,15 @@ RSpec.describe PermissionChecker do
       before do
         uuids.each { |uuid| set_material_owner(uuid, owner) }
       end
+
       it 'should return false' do
-        expect(PermissionChecker.check(:edit, ['xyz', 'zyx'], uuids)).to eq(false)
+        expect(PermissionChecker.check(:edit, ['xyz@sanger.ac.uk', 'zyx@sanger.ac.uk'], uuids)).to eq(false)
       end
+
       it 'should put the unpermitted materials into the unpermitted_uuids attribute' do
-        PermissionChecker.check(:edit, ['xyz', 'zyx'], uuids)
+        PermissionChecker.check(:edit, ['xyz@sanger.ac.uk', 'zyx@sanger.ac.uk'], uuids)
         expect(PermissionChecker.unpermitted_uuids).to eq(uuids)
       end
     end
-
   end
-
 end
